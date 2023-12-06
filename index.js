d1439176ca388fbd5f7394da313c79192cf3ae56
Galledan <onrclklr@outlook.com>
2023-12-06 15:32:49 +0300

    second commit

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const fetchData = async () => {
+
+  const loadEl = document.querySelector(".loading"); // loading divini aldık
+  loadEl.style.display = 'block' // CSS'te display: none var bizde burada gözükmezi için block yaptık
+
+
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/todos?_limit=5"
-  );
+  ); // apidan response aldık
 
   const data = await res.json();
-  console.log(data);
+  console.log(data); //.json ile aldığımız response'ı çevirdik 
 
-  fillData(data);
+  fillData(data); // aldığımız datayı fillData fonksiyonu'na yollayıp doldurduk.
+
+  loadEl.style.display = 'none';
 };
 
-fetchData();
+fetchData(); 
 
 const List = document.querySelector("ul");
 
 const fillData = async (data) => {
+
+  //Aşağıda filldata fonksiyonuna gelen datadaki her telaman için bir li itemı yarattık ve içeriğini doldurduk.
   data.forEach((t) => {
     const liItem = document.createElement("li");
     liItem.innerText = `Title: ${t.title}, Checked: ${t.completed}`;
     List.appendChild(liItem);
   });
+
 };
 
-const handleSubmit = (e) => {
-    e.preventDefault();
+const handleSubmit = (e) => { // Submit için fonksiyon
+    e.preventDefault(); 
 
+  //Api'dan çektiğimiz itemlardan ayrı yeni item eklemek için bir li item oluşturduk ve bunu formdaki inputlarla doldurduk ve listeye ekledik.
   const liItem = document.createElement("li");
   const titleInput = document.querySelector(".titleInput");
   const titleValue = titleInput.value;
@@ -35,4 +46,4 @@ const handleSubmit = (e) => {
 
 const form = document.querySelector(".form-container");
 
-form.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit); // Form'a fonksiyonu ekledik
